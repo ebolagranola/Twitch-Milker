dfeabaaf786af0ac9179c6f0a08a4f8a1ffd9efc
Dale Doster <dale.doster1@marist.edu>
2022-09-05 14:47:41 -0400

    Added observer for initial elements

--- Twitch Milker/script.js
+++ Twitch Milker/script.js
@@ -1,12 +1,10 @@
-// let firstMilking = true;
+let firstRun = true;
 let delayClick = 20000;
 let delayPoint = 6500;
-let lastClick = 0;
-let lastPoint = 0;
-// let perfStart;
-let lastPointVal = null;
+let lastClick = lastPoint = 0;
 let dt24 = false;
 let globalTime = { hour: "", min: "", sec: "", amPm: "" };
+let observerOptions = { childList: true, subtree: true };
 
 function addLeadingZero(str) {
   return str < 10 ? "0" + str : str;
@@ -22,46 +20,48 @@ console.logTime = function(msg) {
   globalTime.min = addLeadingZero(globalTime.min);
   globalTime.sec = addLeadingZero(globalTime.sec);
   globalTime.amPm = (dt24) ? "" : (globalTime.hour < 12 ? "am" : "pm");
-
   console.log("%c" + globalTime.hour + ":" + globalTime.min + ":" + globalTime.sec + globalTime.amPm + ":%c " + msg, "font-size: 32px; color: cyan", "font-size: 32px; color: pink");
 }
 
 let buttonObserver = new MutationObserver(function(mutationList) {
   mutationList.forEach(function(mutation) {
     if (document.contains(document.querySelector('[data-test-selector="community-points-summary"]:not([disabled])') && document.querySelector('button[aria-label="Claim Bonus"]')) && (lastClick + delayClick) < Date.now()) {
-      // if (!firstMilking) console.logTime(Math.round(((window.performance.now() - perfStart)/1000)/60).toString() + " minutes since last milking");
-      // firstMilking = false;
       lastClick = Date.now();
-
       let randomWaitTime = (Math.random() * 9197) + 2394;
       console.logTime("waiting " + Math.round(randomWaitTime) + "ms before milking");
 
       setTimeout(function() {
         document.querySelector('button[aria-label="Claim Bonus"]').click();
-        // perfStart = window.performance.now();
-        // console.logTime("got milk");
       }, randomWaitTime);
     }
   });
 });
 
-buttonObserver.observe(document, { childList: true, subtree: true });
-
 let pointObserver = new MutationObserver(function(mutationList) {
   mutationList.forEach(function(mutation) {
     if (mutation.type == "childList" && document.contains(document.querySelector('.community-points-summary__points-add-text'))) {
       let pointVal = document.querySelector('.community-points-summary__points-add-text').innerText;
-      pointVal = pointVal.slice(1);
-
       if (lastPoint + delayPoint < Date.now()) {
-        console.logTime("got " + pointVal + " milk cartons");
+        console.logTime(pointVal + " milk cartons");
         lastPoint = Date.now();
       }
     }
   });
 });
 
-pointObserver.observe(document, { childList: true, subtree: true });
+let communitySectionObserver = new MutationObserver(function(mutationList) {
+  if (firstRun) {
+    mutationList.forEach(function(mutation) {
+      let communitySection = document.querySelector('[data-test-selector="community-points-summary"]:not([disabled])');
+      if (mutation.type == "childList" && communitySection && firstRun) {
+        firstRun = false;
+        buttonObserver.observe(communitySection, observerOptions);
+        pointObserver.observe(communitySection, observerOptions);
+        console.logTime("started milking");
+      }
+    });
+  }
+});
 
+communitySectionObserver.observe(document.body, observerOptions);
 console.log("%crunning twich %cmilker", "font-size: 64px; color: cyan", "font-size: 64px; color: pink");
-console.logTime("started milking");
