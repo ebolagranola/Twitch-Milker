63b8cc0d641afcdbceb64657ea13f251090ed7b4
Dale Doster <dale.doster1@marist.edu>
2022-09-04 21:56:03 -0400

    Improved console interaction and messaging

--- Twitch Milker/script.js
+++ Twitch Milker/script.js
@@ -1,38 +1,67 @@
 let firstMilking = true;
+let delayClick = 20000;
+let delayPoint = 6500;
 let lastClick = 0;
-let delay = 20000;
-let start;
+let lastPoint = 0;
+let perfStart;
+let lastPointVal = null;
+let dt24 = false;
+let globalTime = { hour: "", min: "", sec: "", amPm: "" };
 
-function getTime() {
-  const date = new Date();
-  let hours = date.getHours();
-  let mins = date.getMinutes();
-  let currHours = hours <= 12 ? hours : hours - 12;
-  let currMins = mins < 10 ? "0" + mins : mins;
-  let amPm = hours < 12 ? "am" : "pm";
-  return currHours + ":" + currMins + amPm;
+function addLeadingZero(str) {
+  return str < 10 ? "0" + str : str;
 }
 
-let observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    if (document.contains(document.querySelector('[data-test-selector="community-points-summary"]:not([disabled])') && document.querySelector('button[aria-label="Claim Bonus"]')) && (lastClick + delay) < Date.now()) {
-      if (!firstMilking) console.log(Math.round(((window.performance.now() - start)/1000)/60).toString(), "minutes since last milking");
+console.logTime = function(msg) {
+  const date = new Date();
+  globalTime.hour = date.getHours();
+  globalTime.min = date.getMinutes();
+  globalTime.sec = date.getSeconds();
+  globalTime.hour = (dt24) ? globalTime.hour : (globalTime.hour <= 12 ? globalTime.hour : globalTime.hour - 12);
+  globalTime.min = addLeadingZero(globalTime.min);
+  globalTime.sec = addLeadingZero(globalTime.sec);
+  globalTime.amPm = (dt24) ? "" : (globalTime.hour < 12 ? "am" : "pm");
+
+  console.log("%c" + globalTime.hour + ":" + globalTime.min + ":" + globalTime.sec + globalTime.amPm + ":%c " + msg, "font-size: 32px; color: cyan", "font-size: 32px; color: pink");
+}
+
+let buttonObserver = new MutationObserver(function(mutationList) {
+  mutationList.forEach(function(mutation) {
+    if (document.contains(document.querySelector('[data-test-selector="community-points-summary"]:not([disabled])') && document.querySelector('button[aria-label="Claim Bonus"]')) && (lastClick + delayClick) < Date.now()) {
+      if (!firstMilking) console.logTime(Math.round(((window.performance.now() - perfStart)/1000)/60).toString() + " minutes since last milking");
       firstMilking = false;
       lastClick = Date.now();
 
       let randomWaitTime = (Math.random() * 9197) + 2394;
-      console.log("waiting", Math.round(randomWaitTime), "ms");
+      console.logTime("new button to click");
+      console.logTime("waiting " + Math.round(randomWaitTime) + "ms before click");
 
       setTimeout(function() {
         document.querySelector('button[aria-label="Claim Bonus"]').click();
-        start = window.performance.now();
-        console.log("got milk at", getTime());
+        perfStart = window.performance.now();
+        console.logTime("got milk");
       }, randomWaitTime);
     }
   });
 });
 
-observer.observe(document, { childList: true, subtree: true });
+buttonObserver.observe(document, { childList: true, subtree: true });
 
-console.log("%crunning twich milker", "font-size: 64px;");
-console.log("started milking at", getTime());
+let pointObserver = new MutationObserver(function(mutationList) {
+  mutationList.forEach(function(mutation) {
+    if (mutation.type == "childList" && document.contains(document.querySelector('.community-points-summary__points-add-text'))) {
+      let pointVal = document.querySelector('.community-points-summary__points-add-text').innerText;
+      pointVal = pointVal.slice(1);
+
+      if (lastPoint + delayPoint < Date.now()) {
+        console.logTime("received " + pointVal + " points");
+        lastPoint = Date.now();
+      }
+    }
+  });
+});
+
+pointObserver.observe(document, { childList: true, subtree: true });
+
+console.log("%crunning twich %cmilker", "font-size: 64px; color: cyan", "font-size: 64px; color: pink");
+console.logTime("started milking");
